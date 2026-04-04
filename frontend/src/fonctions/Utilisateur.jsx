4a0e6a92b61248dbbbfea5f1690c44f7869d4b1d
Kambou oziel <ozielkambou602@gmail.com>
2026-04-04 23:37:13 +0000

    Ajout des fonctions d'interactions des utilisateurs dans le front

---
+++ frontend/src/fonctions/Utilisateur.jsx
@@ -0,0 +1,101 @@
+import axios from 'axios';
+const API_URL = 'http://localhost:3000';
+
+export const getAllUsers = async () => {
+    try {
+        const response = await axios.get(`${API_URL}/user/allUser`);
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error.message;
+    }
+};
+
+export const getUserById = async (id) => {
+    try {
+        const response = await axios.get(`${API_URL}/user/specificUser/${id}`);
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error.message;
+    }
+};
+
+export const newUser = async (userData) => {
+    try {
+        const response = await axios.post(`${API_URL}/user/newUser`, userData);
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error.message;
+    }
+};
+
+export const updateUserForAdmin = async (id, userData) => {
+    try {
+        const response = await axios.put(`${API_URL}/user/updateUserForAdmin/${id}`, userData);
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error.message;
+    }
+};
+
+export const updateSimpleUser = async (id, userData) => {
+    try {
+        const response = await axios.put(`${API_URL}/user/updateUser/${id}`, userData);
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error.message;
+    }
+};
+
+export const deleteUser = async (id) => {
+    try {
+        const response = await axios.delete(`${API_URL}/user/deleteUser/${id}`);
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error.message;
+    }
+};
+
+export const connexion = async (email, mdp) => {
+    try {
+        const response = await axios.post(`${API_URL}/user/connexion`, { email, mdp });
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error.message;
+    }
+};
+
+export const deconnexion = async () => {
+    try {
+        const response = await axios.post(`${API_URL}/user/deconnexion`);
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error.message;
+    }
+};
+
+export const verifierAuthentification = async () => {
+    try {
+        const response = await axios.get(`${API_URL}/user/verifierAuthentification`);
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error.message;
+    }
+};
+
+export const motdepasseOublie = async (email) => {
+    try {
+        const response = await axios.post(`${API_URL}/user/motdepasseOublie`, { email });
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error.message;
+    }
+};
+
+export const changerMotdepasse = async (token, nouveauMdp) => {
+    try {
+        const response = await axios.put(`${API_URL}/user/reinitialiserMotdepasse/${token}`, { nouveauMdp });
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error.message;
+    }
+};
